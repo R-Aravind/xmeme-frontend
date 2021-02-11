1fd9370d724a234c3e11d0b16d27b56573ba54a6
r-aravind <aravind.ar786@gmail.com>
2021-02-11 19:17:17 +0530

    fix: label must have associated control in Form.js

--- src/Form.js
+++ src/Form.js
@@ -28,12 +28,12 @@ const Basic = (props) => (
      >
        {({ isSubmitting }) => (
          <Form className="form">
-            <label className="">Username: </label>
-           <Field type="text" name="name" className="form-input"/>
-           <label className="mt-5">Caption: </label>
-           <Field type="text" name="caption" className="form-input"/>
-           <label className="mt-5">Meme URL: </label>
-           <Field type="text" name="url" className="form-input"/>
+            <label className="" htmlFor="form-name">Username: </label>
+           <Field type="text" id="form-name" name="name" className="form-input"/>
+           <label className="mt-5" htmlFor="form-caption">Caption: </label>
+           <Field type="text" id="form-caption" name="caption" className="form-input"/>
+           <label className="mt-5" htmlFor="form-url">Meme URL: </label>
+           <Field type="text" id="form-url" name="url" className="form-input"/>
            <ErrorMessage name="url" component="div" />
            <button type="submit" disabled={isSubmitting} className="form-button">
              Submit
